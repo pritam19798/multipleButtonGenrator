f606bf7a673a948ff307f596a28f39d630ea1119
pritam19798 <pritamchowdhury19798@gmail.com>
2020-07-07 15:14:32 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'multipleButtonGenrator';
+  sliderValue:number=1;
+  button:number[]=[]
+  clicked:boolean=false
+  ngOnInit(){
+
+  }
+  sliderClick(){
+    console.log(this.sliderValue)
+    this.button=[]
+    this.clicked=true
+    for (let index = 1; index < this.sliderValue+1; index++) {
+      this.button.push(index);
+    }
+  }
+  buttonOnClick(index:number){
+    alert(`you clicked ${index}`)
+  }
 }
